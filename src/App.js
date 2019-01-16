38ac3e92709925f03dc677cb7fcd740619384e1d
saiidalhalawi <saiid.kk@gmail.com>
2019-01-17 01:42:20 +0900

    Use ApolloClient

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
 import React, { Component } from 'react';
+import { ApolloProvider } from 'react-apollo';
+import gql from 'graphql-tag';
+import { Query } from 'react-apollo';
 import './App.css';
+import client from './client'
+
+const ME = gql`
+  query me {
+    user(login: "saiidalhalawi") {
+      name
+      avatarUrl
+    }
+  }
+`;
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <span>Hello GraphQL</span>
-      </div>
+      <ApolloProvider client={client}>
+        <div className="App">
+          <span>Hello GraphQL</span>
+          <Query query={ME}>
+            {
+              ({ loading, error, data }) => {
+                if (loading) { return 'loading...' }
+                if (error) { return `Error: ${error.message}` }
+
+                return <div>{data.user.name}</div>
+              }
+            }
+          </Query>
+        </div>
+      </ApolloProvider>
     );
   }
 }
