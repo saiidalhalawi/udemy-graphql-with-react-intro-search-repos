7c4f7eb0d35c2f307c2463d8e352eb52fa3c1f2c
saiidalhalawi <saiid.kk@gmail.com>
2019-01-18 00:53:07 +0900

    Use another query to search repositories

--- src/App.js
+++ src/App.js
@@ -4,22 +4,33 @@ import { Query } from 'react-apollo';
 
 import './App.css';
 import client from './client';
-import { ME } from './graphql';
+import { SEARCH_REPOSITORIES } from './graphql';
 
+const VARIABLES = {
+  first: 5,
+  after: null,
+  last: null,
+  before: null,
+  query: 'フロントエンドエンジニア'
+};
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = VARIABLES;
+  }
   render() {
+    const { query, first, before, last, after } = this.state;
     return (
       <ApolloProvider client={client}>
         <div className="App">
-          <span>Hello GraphQL</span>
-          <Query query={ME}>
+          <Query query={SEARCH_REPOSITORIES} variables={{ query, first, before, last, after }}>
             {
               ({ loading, error, data }) => {
                 if (loading) { return 'loading...' }
                 if (error) { return `Error: ${error.message}` }
-
-                return <div>{data.user.name}</div>
+                console.log(data);
+                return <div>aaa</div>
               }
             }
           </Query>
