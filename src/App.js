3408ef9bb1dd93917b73d63ec86be1ca2aa6c25e
saiidalhalawi <saiid.kk@gmail.com>
2019-01-18 01:20:30 +0900

    Display repositories-count

--- src/App.js
+++ src/App.js
@@ -2,11 +2,10 @@ import React, { Component } from 'react';
 import { ApolloProvider } from 'react-apollo';
 import { Query } from 'react-apollo';
 
-import './App.css';
 import client from './client';
 import { SEARCH_REPOSITORIES } from './graphql';
 
-const VARIABLES = {
+const DEFAULT_STATE = {
   first: 5,
   after: null,
   last: null,
@@ -14,26 +13,53 @@ const VARIABLES = {
   query: 'フロントエンドエンジニア'
 };
 
+const styles = {
+  main: {
+    textAlign: 'center',
+    marginTop: 15,
+    marginBottom: 15
+  },
+  input: {
+    padding: 20,
+    fontSize: 16,
+    width: '50%'
+  }
+};
+
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = VARIABLES;
+    this.state = DEFAULT_STATE;
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange(event) {
+    this.setState({
+      ...DEFAULT_STATE,
+      query: event.target.value
+    });
   }
   render() {
     const { query, first, before, last, after } = this.state;
     return (
       <ApolloProvider client={client}>
-        <div className="App">
-          <Query query={SEARCH_REPOSITORIES} variables={{ query, first, before, last, after }}>
-            {
-              ({ loading, error, data }) => {
-                if (loading) { return 'loading...' }
-                if (error) { return `Error: ${error.message}` }
-                console.log(data);
-                return <div>aaa</div>
+        <div style={styles.main}>
+          <form>
+            <input value={query} onChange={this.handleChange} style={styles.input} />
+          </form>
+          <div>
+            <Query query={SEARCH_REPOSITORIES} variables={{ query, first, before, last, after }}>
+              {
+                ({ loading, error, data }) => {
+                  if (loading) { return 'loading...' }
+                  if (error) { return `Error: ${error.message}` }
+                  const repositoryCount = data.search.repositoryCount;
+                  const repositoryUnit = repositoryCount === 1 ? 'Repository' : 'Repositories';
+                  const title = `Github Repositories Search Results - ${repositoryCount} ${repositoryUnit}`;
+                  return <h2>{title}</h2>
+                }
               }
-            }
-          </Query>
+            </Query>
+          </div>
         </div>
       </ApolloProvider>
     );
