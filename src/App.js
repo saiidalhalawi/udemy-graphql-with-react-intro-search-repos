5a7a675415e0517a2fa7583b0b5efe9f05bf58d1
saiidalhalawi <saiid.kk@gmail.com>
2019-01-21 01:08:16 +0900

    Use callback to fetch the result of mutation

--- src/App.js
+++ src/App.js
@@ -16,7 +16,26 @@ const StarButton = props => {
         onClick={
           () => {
             addOrRemoveStar({
-              variables: { input: { starrableId: node.id } }
+              variables: { input: { starrableId: node.id } },
+              update: (store, { data: { addStar, removeStar }}) => {
+                const { starrable } = addStar || removeStar;
+                const data = store.readQuery({
+                  query: SEARCH_REPOSITORIES,
+                  variables: { query, first, before, last, after }
+                });
+                const edges = data.search.edges;
+                edges.map(edge => {
+                  if (edge.node.id === node.id) {
+                    const totalCount = edge.node.stargazers.totalCount;
+                    const diff = starrable.viewerHasStarred ? -1 : 1;
+                    const newTotalCount = totalCount + diff;
+                    edge.node.stargazers.totalCount = newTotalCount;
+                  }
+                  return edge;
+                });
+                data.search.edges = edges;
+                store.writeQuery({query: SEARCH_REPOSITORIES, data});
+              }
             });
           }
         }
@@ -28,15 +47,7 @@ const StarButton = props => {
   return (
     <Mutation
       mutation={viewerHasStarred ? REMOVE_STAR : ADD_STAR}
-      refetchQueries={ mutationResult => {
-        console.log(mutationResult);
-        return [
-          {
-            query: SEARCH_REPOSITORIES,
-            variables: { query, first, before, last, after }
-          }
-        ];
-      }}
+
     >
       {
         addOrRemoveStar => <StarStatus addOrRemoveStar={addOrRemoveStar}/>
